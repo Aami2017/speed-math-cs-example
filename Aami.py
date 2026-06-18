e011b620f8015d23a7d1c7d04e4d3316311e6921
Aami2017 <aadhyakrishna2017@gmail.com>
2026-06-18 16:02:48 -0700

    Create Aami.py

---
+++ Aami.py
@@ -0,0 +1,7 @@
+print("la la da da")
+print("Amy\nDhanya\nPrem\nLisi")
+name=input("favorite song?")
+print(f"yor favorite song is {name}")
+print(f"evryone's {name}")
+place="  Redmond  "
+print(place)
